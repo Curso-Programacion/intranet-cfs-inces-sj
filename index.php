88d96bcb6473cea3139726ff3c88e2505ff86dd4
Juan Jose Moreno <j_j_m_a0904@hotmail.com>
2015-08-21 04:55:51 -0430

    Diseñando la index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include("application/views/index/ui/header.htm"); ?>
 <?php include("application/views/index/ui//nav.htm"); ?>
 <div id="content"></div>
+
 <?php include("application/views/index/ui/deleteconfirmmodal.htm"); ?>
 <?php include("application/views/index/ui/footer.htm"); ?>
