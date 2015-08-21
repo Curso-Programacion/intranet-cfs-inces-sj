e4ecc9f5ceaa00e47bc5429990e81f389ffa1c65
Juan Jose Moreno <j_j_m_a0904@hotmail.com>
2015-08-21 01:37:24 -0430

    Re-organizacion de todo el arbol de directorio

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("application/views/index/ui/header.htm"); ?>
+<?php include("application/views/index/ui//nav.htm"); ?>
+<div id="content"></div>
+<?php include("application/views/index/ui/deleteconfirmmodal.htm"); ?>
+<?php include("application/views/index/ui/footer.htm"); ?>
